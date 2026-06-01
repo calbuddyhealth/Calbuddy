a723de5d7059fb564c2f724b93b0a5c35b20ee8a
calbuddyhealth <onofreerostico@yahoo.com>
2026-06-01 10:14:23 -0700

    Change Supabase client initialization method

--- supabase-config.js
+++ supabase-config.js
@@ -3,4 +3,4 @@ const SUPABASE_URL = 'https://qmyrfdhveqqkhsynhzci.supabase.co' ;
 const SUPABASE_ANON_KEY = 'sb_publishable_Ol6fATXdLGiQiEKnImBwlA_Zq4544KA';
 
 // Initialize Supabase client
-const supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
+window.calbuddySupabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
