fd758fc672cc2bf27bcc87ece62a8b67a5a53b8d
calbuddyhealth <onofreerostico@yahoo.com>
2026-08-14 17:57:36 -0700

    Delete retired mixed Ari user action module

--- js/ari-user-actions.js
+++
@@ -1,8 +0,0 @@
-// ARI XP compatibility loader.
-// Legacy mixed meal/workout action logic was retired in favor of
-// domain-specific action services.
-//
-// Meals: ari/actions/ari-meal-action.js
-// Training: ari/actions/ari-workout-plan-action.js (loaded by home.html)
-
-import "../ari/actions/ari-meal-action.js";
