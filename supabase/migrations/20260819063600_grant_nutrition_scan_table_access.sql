4286a54aa474bf5600b57b475efad6dfa41cdd69
calbuddyhealth <onofreerostico@yahoo.com>
2026-08-18 23:38:57 -0700

    Grant Nutrition scan table access

---
+++ supabase/migrations/20260819063600_grant_nutrition_scan_table_access.sql
@@ -0,0 +1,4 @@
+grant select, insert on table public.nutrition_label_evidence to authenticated;
+grant select on table public.ari_nutrition_vision_usage to authenticated;
+grant all on table public.nutrition_label_evidence to service_role;
+grant all on table public.ari_nutrition_vision_usage to service_role;
