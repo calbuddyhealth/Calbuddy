c9e3c64fda45e56ff9ce369878d0f6b09260f26b
calbuddyhealth <onofreerostico@yahoo.com>
2026-08-24 06:00:16 -0700

    Load cleaned Profile renderer

--- js/ari-circle/profile/profile-v3-loader.js
+++ js/ari-circle/profile/profile-v3-loader.js
@@ -181,7 +181,7 @@ function finishProfessionalProfileBoot() {
 }
 
 Promise.all([
-  import("./profile-v4.js?v=4.3.0"),
+  import("./profile-v4.js?v=4.3.1"),
   import("../v4-ui.js?v=5.3.0"),
   import("./profile-social-flow.js?v=1.0.0")
 ])
