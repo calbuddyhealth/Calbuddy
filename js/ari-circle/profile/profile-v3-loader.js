386e8924a224f5f1cd713f6f9c87b6bddcde0d3c
calbuddyhealth <onofreerostico@yahoo.com>
2026-08-24 21:22:41 -0700

    Cache-bust visitor profile connection authority

--- js/ari-circle/profile/profile-v3-loader.js
+++ js/ari-circle/profile/profile-v3-loader.js
@@ -184,7 +184,7 @@ Promise.all([
   import("./profile-v4.js?v=4.3.1"),
   import("../v4-ui.js?v=5.3.2"),
   import("./profile-social-flow.js?v=1.0.0"),
-  import("./profile-connection-authority.js?v=1.0.0")
+  import("./profile-connection-authority.js?v=1.0.1")
 ])
   .then(() => finishProfessionalProfileBoot())
   .catch((error) => {
