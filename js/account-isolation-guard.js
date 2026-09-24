03eed3cbb3584492dfe1c88fcb8d13d593929b00
calbuddyhealth <onofreerostico@yahoo.com>
2026-09-24 00:23:41 -0700

    Bump account isolation guard for visual-state cleanup

--- js/account-isolation-guard.js
+++ js/account-isolation-guard.js
@@ -4,7 +4,7 @@
 (() => {
   "use strict";
 
-  const VERSION = "1.0.0";
+  const VERSION = "1.0.1";
   const ACTIVE_SESSION_KEY = "arixp_active_user_id_v1";
   const STORAGE_OWNER_KEY = "arixp_browser_state_owner_v1";
   const SCOPED_PREFIX = "arixp:u:";
