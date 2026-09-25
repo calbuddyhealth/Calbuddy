309d9cb41c0ebcce66347135c653632975d2a331
calbuddyhealth <onofreerostico@yahoo.com>
2026-09-25 15:18:13 -0700

    Load the new Profile showcase and navigation shell

--- js/ari-circle/profile/profile-v3-loader.js
+++ js/ari-circle/profile/profile-v3-loader.js
@@ -186,9 +186,9 @@ function finishProfessionalProfileBoot() {
 
 Promise.all([
   import("./profile-v4.js?v=4.4.0"),
-  import("../v4-ui.js?v=5.5.0"),
+  import("../v4-ui.js?v=5.6.0"),
   import("./profile-friends.js?v=1.0.0"),
-  import("./profile-gallery-v1.js?v=1.3.0")
+  import("./profile-gallery-v1.js?v=2.0.0")
 ])
   .then(() => finishProfessionalProfileBoot())
   .catch((error) => {
